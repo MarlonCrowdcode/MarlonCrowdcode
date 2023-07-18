06cff58e136e38b633411dcc91308ce628231ef0
MarlonCrowdcode <139772115+MarlonCrowdcode@users.noreply.github.com>
2023-07-18 08:02:02 +0200

    Conitel commit

---
+++ demo/src/main/java/com/example/demo/HelloApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class HelloApplication{
+public static void main(String[] args) {
+
+int i = 5;
+int o = i +4;
+System.out.println("Hallo");
+System.out.println(o+i);
+
+}
+}
